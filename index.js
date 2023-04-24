24ce4d16b029e9244437ad45779543ca066ee685
Krn <krnnkhatri@gmail.com>
2023-04-24 16:49:31 +0530

    added maze generation and game spirit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body } = Matter;
 
-const cells = 5;
+const cells = 25;
 const width = 600;
 const height = 600;
+const unitLength = height/cells;
 
 const engine = Engine.create();
+engine.world.gravity.y=0;
 const { world } = engine;
 const render = Render.create({
     element:document.body,
@@ -22,10 +24,10 @@ Runner.run(Runner.create(), engine);
 // Walls
 
 const walls = [
-    Bodies.rectangle(width/2, 0, width, 40, {isStatic : true}),
-    Bodies.rectangle(width/2, height, width, 40, {isStatic : true}),
-    Bodies.rectangle(0, height/2, 40, height, {isStatic : true}),
-    Bodies.rectangle(width, height/2, 40, height, {isStatic : true})
+    Bodies.rectangle(width/2, 0, width, 2, {isStatic : true}),
+    Bodies.rectangle(width/2, height, width, 2, {isStatic : true}),
+    Bodies.rectangle(0, height/2, 2, height, {isStatic : true}),
+    Bodies.rectangle(width, height/2, 2, height, {isStatic : true})
 ];
 
 World.add(world, walls);
@@ -82,15 +84,90 @@ const stepThroughCell = (row, column) =>{
 
         if(direction === "left") {
             verticals[row][column-1] = true;
-        }  else if(direction === "right"){
+        }  else if(direction === "right") {
             verticals[row][column] = true;
+        }   else if(direction === "up") {
+            horizontals[row -1][column] = true;
+        }   else if(direction === "down") {
+            horizontals[row][column] = true;
         }
 
-
+        stepThroughCell(nextRow, nextColumn);
     }
 
 };
 
 stepThroughCell(startRow, startColumn);
 
-console.log(grid);
+horizontals.forEach((row, rowIndex)=>{
+    row.forEach((open, columnIndex)=>{
+        if(open) {
+            return;
+        }
+        const wall= Bodies.rectangle(
+            columnIndex * unitLength + unitLength / 2,
+            rowIndex * unitLength + unitLength,
+            unitLength,
+            4,
+            {
+                isStatic : true
+            }
+        );
+        World.add(world, wall);
+    });
+});
+
+verticals.forEach((row, rowIndex)=>{
+    row.forEach((open, columnIndex)=>{
+        if(open) {
+            return;
+        }
+        const wall= Bodies.rectangle(
+            columnIndex * unitLength + unitLength,
+            rowIndex * unitLength + unitLength / 2,
+            4,
+            unitLength,
+            {
+                isStatic : true
+            }
+        );
+        World.add(world, wall);
+    });
+});
+
+
+const goal = Bodies.rectangle(
+    width - unitLength / 2,
+    height - unitLength / 2,
+    unitLength * .7,
+    unitLength * .7,
+    {
+        isStatic : true
+    }
+);
+World.add(world, goal);
+
+const ball = Bodies.circle(
+    unitLength / 2,
+    unitLength / 2,
+    unitLength / 4 ,
+);
+
+World.add(world, ball);
+
+document.addEventListener("keydown", event => {
+    const {x, y} = ball.velocity;
+
+    if(event.keyCode === 87) {
+        Body.setVelocity(ball, {x, y : y - 5});
+    }
+    if(event.keyCode === 68) {
+        Body.setVelocity(ball, {x : x + 5, y});
+    }
+    if(event.keyCode === 83) {
+        Body.setVelocity(ball, {x , y : y + 5});
+    }
+    if(event.keyCode === 65) {
+        Body.setVelocity(ball, {x : x - 5, y});
+    }
+});
